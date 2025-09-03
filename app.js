db1cef72216c1261ed24778b46ff3c30e8403e68
jaswinder Kumar <kumarsanju54@gmail.com>
2025-09-03 16:45:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('this should work 🚀\n');
+  res.end('this should work i sthis working now 🚀\n');
 });
 
 const PORT = 3000;
